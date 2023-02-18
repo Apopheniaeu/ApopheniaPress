de77297e13173cb8c5441fa6d82437a2df830669
Apopheniaeu <53937282+Apopheniaeu@users.noreply.github.com>
2023-02-18 15:52:49 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import "./styles.css";
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setScreenWidth(window.innerWidth);
+    }
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <div className="pulsating-text">
+      {screenWidth > 768 ? (
+        <>
+          <a
+            href="mailto:press@apophenia.eu"
+            style={{ color: "#faf5ed", textDecoration: "none" }}
+          >
+            apophenia
+          </a>{" "}
+          . <br />
+          <a
+            href="mailto:press@apophenia.eu"
+            style={{ color: "#faf5ed", textDecoration: "none" }}
+          >
+            press -----
+          </a>{" "}
+          <br />
+          <a
+            href="mailto:press@apophenia.eu"
+            style={{ color: "#faf5ed", textDecoration: "none" }}
+          >
+            .......................
+          </a>
+        </>
+      ) : (
+        <>
+          <a
+            href="mailto:press@apophenia.eu"
+            style={{ color: "#faf5ed", textDecoration: "none" }}
+          >
+            a
+          </a>
+          <br />
+          <a
+            href="mailto:press@apophenia.eu"
+            style={{ color: "#faf5ed", textDecoration: "none" }}
+          >
+            p
+          </a>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
