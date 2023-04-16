fa1df06d70a87140f3093ba31ec6427289b4c9f8
apopheniaeu <info@apophenia.eu>
2023-04-16 22:14:12 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [fontVariation, setFontVariation] = useState(250);
 
   useEffect(() => {
     function handleResize() {
@@ -19,19 +19,16 @@ function App() {
     const { pageY } = event;
     const { innerHeight } = window;
     const percentage = pageY / innerHeight;
-    const fontVariation = Math.round(200 + percentage * 100);
-    document.documentElement.style.setProperty("--font-variation", fontVariation);
-    const mouseY = event.clientY;
-    const middle = window.innerHeight / 2;
-    const difference = mouseY - middle;
-    const range = middle / 2;
-    const normalizedDifference = difference / range;
-    const fontVariationInRange = Math.round(250 + 50 * normalizedDifference);
-    document.documentElement.style.setProperty("--font-variation", fontVariationInRange);
+    const newFontVariation = Math.round(200 + percentage * 100);
+    setFontVariation(newFontVariation);
   }
 
   return (
-    <div className="pulsating-text" onMouseMove={handleMouseMove}>
+    <div
+      className="pulsating-text"
+      onMouseMove={handleMouseMove}
+      style={{ fontVariationSettings: `"grid" ${fontVariation}` }}
+    >
       {screenWidth > 768 ? (
         <>
           <a
